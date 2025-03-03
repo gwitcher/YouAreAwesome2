842ca5106fe3b840450af9b130bfd26c3cbe1c74
Gabe Witcher <gabewitcher@gmail.com>
2025-03-03 13:30:21 -0800

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Gabe Witcher on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
